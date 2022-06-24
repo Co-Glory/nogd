ad9d6a900588b9676972570a4d010ec460761fd6
Mo salah <MSalahElrayes@gmail.com>
2022-06-24 14:31:03 +0200

    'logger'

--- lib/service/logger.dart
+++ lib/service/logger.dart
@@ -1,12 +1,12 @@
 import 'package:logger/logger.dart';
 
-Logger logger = Logger();
+final logger = Logger();
 
 extension Log on Object {
-  void logD() => logger.d(toString());
-  void logE() => logger.e(toString());
-  void logI() => logger.i(toString());
-  void logW() => logger.w(toString());
-  void logV() => logger.v(toString());
-  void logWtf() => logger.wtf(toString());
+  void logD([dynamic message]) => logger.d(toString(), message);
+  void logE([dynamic message]) => logger.e(toString(), message);
+  void logI([dynamic message]) => logger.i(toString(), message);
+  void logW([dynamic message]) => logger.w(toString(), message);
+  void logV([dynamic message]) => logger.v(toString(), message);
+  void logWtf([dynamic message]) => logger.wtf(toString(), message);
 }
